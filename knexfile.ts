97c12a130ec8955cf4803640aec61d93bca9a2de
Daniel Palmares <danielpalmares100@gmail.com>
2024-01-27 20:11:28 -0300

    fix: change env name

--- knexfile.ts
+++ knexfile.ts
@@ -4,7 +4,7 @@ dotenv.config();
 export default {
   development: {
     client: 'pg',
-    connection: process.env.PG_CONNECTION_STRING,
+    connection: process.env.DATABASE_URL,
     migrations: {
       tableName: 'knex_migrations',
       directory: `${__dirname}/src/database/migrations`,
