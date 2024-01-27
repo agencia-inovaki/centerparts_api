7dc9dbef422955000ac391aba8c2526a1c4db3b8
Daniel Palmares <danielpalmares100@gmail.com>
2024-01-27 20:08:59 -0300

    test prod

--- knexfile.ts
+++ knexfile.ts
@@ -3,12 +3,8 @@ dotenv.config();
 
 export default {
   development: {
-    client: process.env.DB_CLIENT,
-    connection: {
-      database: process.env.DB_DATABASE_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-    },
+    client: 'pg',
+    connection: process.env.PG_CONNECTION_STRING,
     migrations: {
       tableName: 'knex_migrations',
       directory: `${__dirname}/src/database/migrations`,
