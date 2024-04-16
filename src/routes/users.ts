fa29459d62f34658286d9a44827de922162f4a7b
Daniel Palmares <danielpalmares100@gmail.com>
2024-04-16 15:41:48 -0300

    fix: remove post user route

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import { auth } from '../middleware/auth'
 import { authenticationController } from '../useCases/Users/Authentication'
-import { createUserController } from '../useCases/Users/CreateUser'
+// import { createUserController } from '../useCases/Users/CreateUser'
 import { getUserController } from '../useCases/Users/GetUser'
 
 export const userRouter = express.Router()
@@ -10,9 +10,9 @@ userRouter
   .post('/authenticate', async (request, response) =>
     authenticationController.handle(request, response)
   )
-  .post('/user', async (request, response) =>
-    createUserController.handle(request, response)
-  )
+  // .post('/user', async (request, response) =>
+  //   createUserController.handle(request, response)
+  // )
   .get('/user/:email', auth, async (request, response) =>
     getUserController.handle(request, response)
   )
