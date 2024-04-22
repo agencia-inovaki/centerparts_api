7455740c16410301df5b4a512cbbac5209ab7465
Mario Sergio <mario-osf@hotmail.com>
2024-04-22 16:43:41 -0300

    change cors origin

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ import { productRouter } from './routes/products'
 dotenv.config()
 
 const app = express()
-app.use(cors())
+app.use(cors({origin: "*"}))
 
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ limit: '50mb', extended: false }))
